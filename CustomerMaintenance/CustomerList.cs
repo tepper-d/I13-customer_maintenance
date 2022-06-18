10bbbcbfd1dbdf806f9367e824262e91a3ea5c36
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-06-18 00:50:33 -0700

    comments

--- CustomerMaintenance/CustomerList.cs
+++ CustomerMaintenance/CustomerList.cs
@@ -26,8 +26,50 @@ using System.Text;
  * ************************************************/
 
 namespace CustomerMaintenance
-{
-    internal class CustomerList
-    {
-    }
+{ 
+	public class CustomerList
+	{
+		//5b. private list
+		private List<Customer> customers;
+
+		//6a. constructor
+		public CustomerList()
+		{
+			customers = new List<Customer>();
+		}
+
+		//6c. count property
+		public int Count => customers.Count;
+
+		//6b. indexer
+		public Customer this[int i]
+		{
+			get
+			{
+				return customers[i];
+			}
+			set
+			{
+				customers[i] = value;
+			}
+		}
+		//6d-1. Add(customer)
+		public void Add(Customer customer)
+		{
+			customers.Add(customer);
+		}
+
+		//6d-2. Remove(customer)
+		public void Remove(Customer customer)
+		{
+			customers.Remove(customer);
+		}
+
+		//6d-3. Fill()
+		public void Fill() => customers = CustomerDB.GetCustomers();
+
+		//6d-4. Save()
+		public void Save() => CustomerDB.SaveCustomers(customers);
+
+	}
 }
